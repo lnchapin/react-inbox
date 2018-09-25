2e5900304954b045c4dadae172c1eadb6311c4af
Lindsay Chapin <lnchapin@gmail.com>
2018-09-24 20:58:13 -0600

    audit fix

--- react-inbox/src/components/Messages.js
+++ react-inbox/src/components/Messages.js
@@ -1,89 +1,102 @@
-import React from 'react'
+import React from "react";
 
-const Messages = ({messages, update}) => {
+const Messages = ({ messages, update }) => {
   console.log(messages);
-  const getLabel = labels => labels.map(lbl =>
-    <span className="label label-warning" key={lbl}>{lbl}</span>
-  );
-
+  const getLabel = labels =>
+    labels.map(lbl => (
+      <span className="label label-warning" key={lbl}>
+        {lbl}
+      </span>
+    ));
 
   const getMessageClass = message => {
-    let cls = ''
+    let cls = "";
     if (message.read) {
-      cls += ' read'
+      cls += " read";
     } else {
-      cls += ' unread'
+      cls += " unread";
     }
-    return cls
-  }
+    return cls;
+  };
 
   const getStarStatus = message => {
-    let star = ''
-    if (message.starred){
-      star += 'fa-star'
-    } else{
-      star += 'fa-star-o'
+    let star = "";
+    if (message.starred) {
+      star += "fa-star";
+    } else {
+      star += "fa-star-o";
     }
-    return star
-  }
+    return star;
+  };
 
   const getSelectStatus = message => {
-    let check = ''
+    let check = "";
     if (message.selected) {
-      check += 'checked'
-    } else{
-      check += ''
+      check += "checked";
+    } else {
+      check += "";
     }
-    return check
-  }
+    return check;
+  };
 
   const colorSelected = message => {
-  let highlight = ''
-  if (message.selected) {
-    highlight += ' selected'
-  } else{
-    highlight += ''
-  }
-  return highlight
-}
+    let highlight = "";
+    if (message.selected) {
+      highlight += " selected";
+    } else {
+      highlight += "";
+    }
+    return highlight;
+  };
 
-  const changeStar = message  => {
-    if (message.starred){
+  const changeStar = message => {
+    if (message.starred) {
       message.starred = false;
     } else {
       message.starred = true;
     }
-    update(messages)
-  }
+    update(messages);
+  };
 
-  const changeCheck = message  => {
-    if (message.selected){
+  const changeCheck = message => {
+    if (message.selected) {
       message.selected = false;
     } else {
       message.selected = true;
     }
-    update(messages)
-  }
+    update(messages);
+  };
 
-  return messages.map(message => <div key={message.id} className={"row message " + getMessageClass(message) + colorSelected(message)}>
-        <div className="col-xs-1">
-          <div className="row">
-            <div className="col-xs-2">
-              <input type="checkbox" onChange={changeCheck.bind(this, message)} checked= {getSelectStatus(message)} />
-            </div>
-            <div className="col-xs-2">
-              <i onClick={changeStar.bind(this, message)} className={"star fa " + getStarStatus(message)}></i>
-            </div>
+  return messages.map(message => (
+    <div
+      key={message.id}
+      className={
+        "row message " + getMessageClass(message) + colorSelected(message)
+      }
+    >
+      <div className="col-xs-1">
+        <div className="row">
+          <div className="col-xs-2">
+            <input
+              type="checkbox"
+              onChange={changeCheck.bind(this, message)}
+              checked={getSelectStatus(message)}
+            />
+          </div>
+          <div className="col-xs-2">
+            <i
+              onClick={changeStar.bind(this, message)}
+              className={"star fa " + getStarStatus(message)}
+            />
           </div>
         </div>
-        <div className="col-xs-11">
-          {getLabel(message.labels)}
-          <a href="">
-            {message.subject}
-          </a>
-        </div>
       </div>
-    )
-}
+      <div className="col-xs-11">
+        {getLabel(message.labels)}
+        <a href="">{message.subject}</a>
+      </div>
+    </div>
+  ));
+};
 
-export default Messages
+export default Messages;
